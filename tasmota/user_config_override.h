b4bd9e233d8cac6383500f44faf8c7fd866f87bd
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2022-04-23 10:20:52 +0100

    user_config_override: Fix #define typo
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -104,7 +104,7 @@
 #define USE_MQTT_WATSON_IOT
 #define USE_MQTT_MOSQUITTO // Use our Mosquitto server (which is based on the WIOT changes). Note we can define both and both CA certs will build in
 
-#if !defined(FIRMWARE_MINIMAL) and !defined(FIRMWARE_LITE)
+#if !defined(FIRMWARE_MINIMAL) && !defined(FIRMWARE_LITE)
 
 #if 1
 #ifndef USE_MQTT_TLS
