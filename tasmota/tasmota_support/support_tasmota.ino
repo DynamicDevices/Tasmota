74b02ca799a11caff03aba417b92dff4423b8811
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2023-02-20 19:14:21 +0000

    support_tasmota.ino: Perform MQTTCheck() even when no WiFi
    So this works when we have cellular up
    
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- tasmota/tasmota_support/support_tasmota.ino
+++ tasmota/tasmota_support/support_tasmota.ino
@@ -1615,6 +1615,8 @@ void Every250mSeconds(void)
         // send FUNC_NETWORK_DOWN to all modules
 //        AddLog(LOG_LEVEL_DEBUG_MORE, PSTR("WIF: Sending FUNC_NETWORK_DOWN"));
         XdrvXsnsCall(FUNC_NETWORK_DOWN);
+
+        MqttCheck();
       }                                           // Every x.75 second
     }
     break;
