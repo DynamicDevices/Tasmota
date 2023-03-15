39dc492480759227a83f1366f823db024f118c13
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2023-03-15 20:31:24 +0000

    user_config_override: Ensure TLS is enabled by default
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -134,6 +134,9 @@
 #define MQTT_HOST         "broker-new.kettlecompanion.com"
 //#define MQTT_HOST         "jit4q3.messaging.internetofthings.ibmcloud.com"
 #define MQTT_PORT         8883
+#undef MQTT_TLS_ENABLED
+#define MQTT_TLS_ENABLED       true             // [SetOption103] Enable TLS mode (requires TLS version)
+
 #define MQTT_USER         "use-token-auth"
 #ifndef MQTT_CLIENT_ID
 #define MQTT_CLIENT_ID    "DummyClientId"
