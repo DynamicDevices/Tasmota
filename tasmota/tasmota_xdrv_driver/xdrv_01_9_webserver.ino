f938ab3d66e56089fe691961b6c465dd013b45ab
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2023-03-19 17:49:02 +0000

    xdrv_01_9_webserver: Add KC version to web page footer
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- tasmota/tasmota_xdrv_driver/xdrv_01_9_webserver.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_01_9_webserver.ino
@@ -388,7 +388,7 @@ const char HTTP_COUNTER[] PROGMEM =
   "<br><div id='t' style='text-align:center;'></div>";
 
 const char HTTP_END[] PROGMEM =
-  "<div style='text-align:right;font-size:11px;'><hr/><a href='https://bit.ly/tasmota' target='_blank' style='color:#aaa;'>Tasmota %s " D_BY " Theo Arends</a></div>"
+  "<div style='text-align:right;font-size:11px;'><hr/><a href='https://bit.ly/tasmota' target='_blank' style='color:#aaa;'>Tasmota %s " D_BY " Theo Arends, modified by Kettle Companion %s</a></div>"
   "</div>"
   "</body>"
   "</html>";
@@ -986,7 +986,7 @@ void WSContentStop(void) {
       WSContentSend_P(HTTP_COUNTER);
     }
   }
-  WSContentSend_P(HTTP_END, TasmotaGlobal.version);
+  WSContentSend_P(HTTP_END, TasmotaGlobal.version, TasmotaGlobal.kc_version);
   WSContentEnd();
 }
 
