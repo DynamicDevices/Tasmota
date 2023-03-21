8bc06f37cf522b29efa53ff8a0941f5dae631b98
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2023-03-21 15:35:32 +0000

    xnrg_02_cse7766: Allow override on CSE7766 resistor value
    Different hardware has different voltage divider resistor values
    which was leading to us getting the wrong readings
    
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- tasmota/tasmota_xnrg_energy/xnrg_02_cse7766.ino
+++ tasmota/tasmota_xnrg_energy/xnrg_02_cse7766.ino
@@ -37,7 +37,9 @@
 #define CSE_PULSES_NOT_INITIALIZED  -1
 
 #define CSE_PREF                    1000
+#ifndef CSE_UREF
 #define CSE_UREF                    100
+#endif
 
 #define CSE_BUFFER_SIZE             25
 
